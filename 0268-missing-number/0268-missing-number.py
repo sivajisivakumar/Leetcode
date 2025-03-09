8c6357c9a887bf8f9249a0a8f7e478af2f0e072a
Sivaji Siva Kumar <156265248+sivajisivakumar@users.noreply.github.com>
2025-03-09 15:13:20 +0530

    Time: 2565 ms (17.87%), Space: 13.3 MB (88.68%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(0,len(nums)+1):
+            if i not in nums:
+                return i
